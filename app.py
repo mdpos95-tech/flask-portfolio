46c9b6d02c023ce01b9406e0a038747593a8cd10
mdpos95-tech <mdpos95@gmail.com>
2026-04-13 19:12:51 +0100

    Working on defining Flask app in .py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+projects_list = [
+    "Project 1: Football Team Fan Site",
+    "Project 2: Game of Thrones Quiz Site",
+    "Project 3: Flask Portfolio"
+]
+skills_list = [
+    "Python",
+    "Flask",
+    "HTML",
+    "CSS",
+    "Javascript",
+    "GitHub"
+]
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
 
 if __name__ == '__main__':
-    
+
     app.run(debug=True)
