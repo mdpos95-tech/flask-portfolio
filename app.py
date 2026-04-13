dcacdf7eb6bd43df0fb7adcbbe069da6f7ff569f
mdpos95-tech <mdpos95@gmail.com>
2026-04-13 19:24:47 +0100

    Working on defining Flask app in .py

--- app.py
+++ app.py
@@ -18,6 +18,26 @@ skills_list = [
 def home():
     return render_template('index.html', title='Home')
 
+@app.route("/projects")
+def projects():
+    return render_template('projects.html', title='Projects', projects=projects_list)
+
+@app.route("/skills")
+def skills():
+    return render_template('skills.html', title='Skills', skills=skills_list)
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    message = ""
+    if request.method == 'POST':
+        name = request.form.get('name')
+        message = f"Thanks, {name}! Your message has been received."
+        
+        return render_template('contact.html', title='Contact', message=message)
+
+
 if __name__ == '__main__':
 
     app.run(debug=True)
