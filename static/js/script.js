676d1999c9c7387135305c95ac9c52f7bb64b0cc
mdpos95-tech <mdpos95@gmail.com>
2026-04-14 20:53:05 +0100

    adding JavaScript element to projects page

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('JavaScript is connected');
 });
+
+document.querySelectorAll(".project-card").forEach(card => {
+    const link = 
+    card.getAttribute("data-link");
+    if (link) {
+        card.style.cursor = "pointer";
+        card.addEventListener("click", (e) => {
+            if (e.target.tagName === "A")
+                return;
+            window.open(link, "_blank");
+        });
+            
+            }
+});
