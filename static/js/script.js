661449b29b830d3f53c63712454beed43cc49279
mdpos95-tech <mdpos95@gmail.com>
2026-04-15 15:47:52 +0100

    adding toggle dark mode js

--- static/js/script.js
+++ static/js/script.js
@@ -34,3 +34,18 @@ document.addEventListener('DOMContentLoaded',
 faders.forEach(el => observer.observe(el));
      });
      
+
+     const toggleBtn = document.getElementById('darkModeToggle');
+     if (toggleBtn) {
+         toggleBtn.addEventListener('click', () => {
+             document.body.classList.toggle('dark-mode');
+             if (document.body.classList.contains('dark-mode')) {
+                 localStorage.setItem('theme', 'dark');
+             } else {
+                 localStorage.setItem('theme', 'light');
+             }
+         });    }
+
+         if (localStorage.getItem('theme') === 'dark') {
+             document.body.classList.add('dark-mode');
+         }
