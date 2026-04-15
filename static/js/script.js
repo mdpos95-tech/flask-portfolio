f8a172ddbd2c857c08f737ecc921ea149dead325
mdpos95-tech <mdpos95@gmail.com>
2026-04-15 15:30:02 +0100

    adding fade in js

--- static/js/script.js
+++ static/js/script.js
@@ -15,3 +15,22 @@ document.querySelectorAll(".project-card").forEach(card => {
             
             }
 });
+
+
+document.addEventListener('DOMContentLoaded',
+     () => {
+    const faders = 
+    document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+faders.forEach(el => observer.observe(el));
+     });
+     
